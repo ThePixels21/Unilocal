372bd7244efbde19113109499b65d7d50cf2e4b4
pipe0427 <pipe.04271@gmail.com>
2023-09-19 16:11:58 -0500

    interfaz de login casi terminada, interfaz de registrar usuario terminada, interfaz de main arreglada

--- app/src/main/java/co/edu/eam/proyectounilocal/actividades/LoginActivity.kt
+++ app/src/main/java/co/edu/eam/proyectounilocal/actividades/LoginActivity.kt
@@ -1,12 +1,22 @@
 package co.edu.eam.proyectounilocal.actividades
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import co.edu.eam.proyectounilocal.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+    fun irARegsitro(v: View){
+        val intent = Intent(this, RegistroActivity::class.java)
+        startActivity(intent)
+    }
+    fun irAOlvidoContrasena(v: View){
+        val intent = Intent(this, OlvidoContrasenaActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/co/edu/eam/proyectounilocal/actividades/MainActivity.kt
+++ app/src/main/java/co/edu/eam/proyectounilocal/actividades/MainActivity.kt
@@ -1,12 +1,19 @@
 package co.edu.eam.proyectounilocal.actividades
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import co.edu.eam.proyectounilocal.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun irALogin(v: View){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
