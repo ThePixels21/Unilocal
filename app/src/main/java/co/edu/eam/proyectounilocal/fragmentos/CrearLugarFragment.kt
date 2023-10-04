505d73860d77caee38dd775a1b08e8f02ed148d3
ThePixels21 <santigamer521@gmail.com>
2023-10-03 23:58:22 -0500

    Strings update and places schedule

--- app/src/main/java/co/edu/eam/proyectounilocal/fragmentos/CrearLugarFragment.kt
+++ app/src/main/java/co/edu/eam/proyectounilocal/fragmentos/CrearLugarFragment.kt
@@ -3,6 +3,7 @@ package co.edu.eam.proyectounilocal.fragmentos
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,7 +20,9 @@ import co.edu.eam.proyectounilocal.bd.Lugares
 import co.edu.eam.proyectounilocal.databinding.FragmentCrearLugarBinding
 import co.edu.eam.proyectounilocal.modelo.Categoria
 import co.edu.eam.proyectounilocal.modelo.Ciudad
+import co.edu.eam.proyectounilocal.modelo.DiaSemana
 import co.edu.eam.proyectounilocal.modelo.EstadoLugar
+import co.edu.eam.proyectounilocal.modelo.Horario
 import co.edu.eam.proyectounilocal.modelo.Lugar
 
 class CrearLugarFragment : Fragment() {
@@ -41,6 +44,7 @@ class CrearLugarFragment : Fragment() {
         ciudades = Ciudades.listar()
         categorias = Categorias.listar()
 
+        cargarHorarios()
         cargarCiudades()
         cargarCategorias()
 
@@ -53,6 +57,30 @@ class CrearLugarFragment : Fragment() {
         binding.btnBuscar.setOnClickListener { startActivity(Intent(requireActivity(), BusquedaActivity::class.java)) }
 
         return binding.root
+    }
+
+    fun cargarHorarios(){
+        val lista: ArrayList<String> = ArrayList()
+        lista.add(getString(R.string.txt_seleccione))
+        for (i in 1..24){
+            lista.add(i.toString())
+        }
+        val adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_item, lista)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.inicioLunes.adapter = adapter
+        binding.finalLunes.adapter = adapter
+        binding.inicioMartes.adapter = adapter
+        binding.finalMartes.adapter = adapter
+        binding.inicioMiercoles.adapter = adapter
+        binding.finalMiercoles.adapter = adapter
+        binding.inicioJueves.adapter = adapter
+        binding.finalJueves.adapter = adapter
+        binding.inicioViernes.adapter = adapter
+        binding.finalViernes.adapter = adapter
+        binding.inicioSabado.adapter = adapter
+        binding.finalSabado.adapter = adapter
+        binding.inicioDomingo.adapter = adapter
+        binding.finalDomingo.adapter = adapter
     }
 
     fun cargarCiudades(){
@@ -86,6 +114,7 @@ class CrearLugarFragment : Fragment() {
     }
 
     fun crearLugar(){
+        Log.e("CrearLugarFragment", binding.ciudadLugar.selectedItem.toString())
         val nombre = binding.nombreLugar.text.toString()
         val descripcion = binding.descripcionLugar.text.toString()
         val telefono = binding.telefonoLugar.text.toString()
@@ -125,6 +154,83 @@ class CrearLugarFragment : Fragment() {
                 val telefonos: ArrayList<String> = ArrayList()
                 telefonos.add(telefono)
                 nuevoLugar.telefonos = telefonos
+
+                if(binding.inicioLunes.selectedItemPosition!=0 && binding.finalLunes.selectedItemPosition != 0){
+                    val horaInicio: Int = binding.inicioLunes.selectedItem.toString().toInt()
+                    val horaFinal: Int = binding.finalLunes.selectedItem.toString().toInt()
+                    if(horaInicio < horaFinal){
+                        val dia: ArrayList<DiaSemana> = ArrayList()
+                        dia.add(DiaSemana.LUNES)
+                        val horario = Horario(0, dia, horaInicio, horaFinal)
+                        nuevoLugar.horarios.add(horario)
+                    }
+                }
+
+                if(binding.inicioMartes.selectedItemPosition!=0 && binding.finalMartes.selectedItemPosition != 0){
+                    val horaInicio: Int = binding.inicioMartes.selectedItem.toString().toInt()
+                    val horaFinal: Int = binding.finalMartes.selectedItem.toString().toInt()
+                    if(horaInicio < horaFinal){
+                        val dia: ArrayList<DiaSemana> = ArrayList()
+                        dia.add(DiaSemana.MARTES)
+                        val horario = Horario(0, dia, horaInicio, horaFinal)
+                        nuevoLugar.horarios.add(horario)
+                    }
+                }
+
+                if(binding.inicioMiercoles.selectedItemPosition!=0 && binding.finalMiercoles.selectedItemPosition != 0){
+                    val horaInicio: Int = binding.inicioMiercoles.selectedItem.toString().toInt()
+                    val horaFinal: Int = binding.finalMiercoles.selectedItem.toString().toInt()
+                    if(horaInicio < horaFinal){
+                        val dia: ArrayList<DiaSemana> = ArrayList()
+                        dia.add(DiaSemana.MIERCOLES)
+                        val horario = Horario(0, dia, horaInicio, horaFinal)
+                        nuevoLugar.horarios.add(horario)
+                    }
+                }
+
+                if(binding.inicioJueves.selectedItemPosition!=0 && binding.finalJueves.selectedItemPosition != 0){
+                    val horaInicio: Int = binding.inicioJueves.selectedItem.toString().toInt()
+                    val horaFinal: Int = binding.finalJueves.selectedItem.toString().toInt()
+                    if(horaInicio < horaFinal){
+                        val dia: ArrayList<DiaSemana> = ArrayList()
+                        dia.add(DiaSemana.JUEVES)
+                        val horario = Horario(0, dia, horaInicio, horaFinal)
+                        nuevoLugar.horarios.add(horario)
+                    }
+                }
+
+                if(binding.inicioViernes.selectedItemPosition!=0 && binding.finalViernes.selectedItemPosition != 0){
+                    val horaInicio: Int = binding.inicioViernes.selectedItem.toString().toInt()
+                    val horaFinal: Int = binding.finalViernes.selectedItem.toString().toInt()
+                    if(horaInicio < horaFinal){
+                        val dia: ArrayList<DiaSemana> = ArrayList()
+                        dia.add(DiaSemana.VIERNES)
+                        val horario = Horario(0, dia, horaInicio, horaFinal)
+                        nuevoLugar.horarios.add(horario)
+                    }
+                }
+
+                if(binding.inicioSabado.selectedItemPosition!=0 && binding.finalSabado.selectedItemPosition != 0){
+                    val horaInicio: Int = binding.inicioSabado.selectedItem.toString().toInt()
+                    val horaFinal: Int = binding.finalSabado.selectedItem.toString().toInt()
+                    if(horaInicio < horaFinal){
+                        val dia: ArrayList<DiaSemana> = ArrayList()
+                        dia.add(DiaSemana.SABADO)
+                        val horario = Horario(0, dia, horaInicio, horaFinal)
+                        nuevoLugar.horarios.add(horario)
+                    }
+                }
+
+                if(binding.inicioDomingo.selectedItemPosition!=0 && binding.finalDomingo.selectedItemPosition != 0){
+                    val horaInicio: Int = binding.inicioDomingo.selectedItem.toString().toInt()
+                    val horaFinal: Int = binding.finalDomingo.selectedItem.toString().toInt()
+                    if(horaInicio < horaFinal){
+                        val dia: ArrayList<DiaSemana> = ArrayList()
+                        dia.add(DiaSemana.DOMINGO)
+                        val horario = Horario(0, dia, horaInicio, horaFinal)
+                        nuevoLugar.horarios.add(horario)
+                    }
+                }
 
                 Lugares.crear(nuevoLugar)
 
